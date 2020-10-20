8d66980f849f25ecfb9dea28af2629e85b8be8fd
Eddie Saliby <eddysaliby@gmail.com>
2020-10-20 16:56:48 +0200

    Added capitalize, camelCase, snake_case, leet

---
+++ exercice-1/string.js
@@ -0,0 +1,51 @@
+function ucfirst(string) {
+    if (typeof string !== 'string')
+        return '';
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+console.log(ucfirst('eddy'));
+
+
+function capitalize(string) {
+    if (typeof string !== 'string')
+        return '';
+    var str = string.split(' ')
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1);     
+    }
+    return str.join(' '); 
+}
+
+console.log(capitalize('eddy salibi'));
+
+function camelCase(string) {
+    if (typeof string !== 'string')
+        return '';
+        var str = string.split(' ')
+        for (var i = 0; i < str.length; i++) {
+            str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1);     
+        }
+        return str.join(''); 
+}
+console.log(camelCase('eddy salibi'));
+
+
+function snakeCase(string) {
+    if (typeof string !== 'string')
+        return '';
+        var str = string.split(' ')
+        for (var i = 0; i < str.length; i++) {
+            str[i] = str[i].charAt(0).toLowerCase() + str[i].substring(1);     
+        }
+        return str.join('_'); 
+}
+console.log(snakeCase('eddy salibi'));
+
+function leet(string) {
+    if (typeof string !== 'string')
+        return '';
+      
+    return string.replace('A', "4").replace('I', "1").replace('O', "o").replace('U', "_").replace('Y', "7")
+}
+console.log(leet('I WANT TO  WU0Y'));
