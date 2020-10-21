18868b5fde334a6bbd5eb32cb55a406d78a91037
Eddie Saliby <eddysaliby@gmail.com>
2020-10-21 18:27:56 +0200

    fiv

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -5,3 +5,17 @@ function type_check_v1(val, type) {
     }
 }
 console.log(type_check_v1(1, 'number'));
+console.log(type_check_v1('edd', 'string'));
+
+
+
+
+function type_check_v2(arg, type,arg2) {
+    if (typeof(arg) === type) {
+        return true
+    }
+    if (arg.value === arg2) {
+        return true
+    }
+}
+console
